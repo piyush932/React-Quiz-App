c874938c261ee834c4706631a1cbe8431c9f3883
Piyush Kumar Nayak <107127794+piyush932@users.noreply.github.com>
2023-09-18 16:16:18 +0530

    Add files via upload

---
+++ src/components/Options.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswered ?
+            index === question.correctOption ? "correct" : "wrong":""
+          }`}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          key={option}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function StartScreen({num,dispatch}) {
+  return (
+    <div className='start'>
+        <h2>Welcome to The React Quiz</h2>
+        <h3>{num} questions to test your React Mastery</h3>
+        <button className='btn btn-ui' onClick={()=>dispatch({type:"start"})}>Let's Start</button>
+    </div>
+  )
+}
